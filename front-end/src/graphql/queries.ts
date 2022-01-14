0e37c1cfbc8152e440c90b53e934a8324af16e3c
Gustavo [kustela] <gustavo_vsilva@outlook.com>
2022-01-14 01:08:20 -0300

    add views structure and styled-components

---
+++ front-end/src/graphql/queries.ts
@@ -0,0 +1,28 @@
+//same query used in apollo playground to get song data
+export const songs = `
+query GetSongs {
+    songs {
+      song
+      artist
+      songReleaseDate
+      playCount
+      metricA
+      metricB
+      metricC
+      metricCi
+      metricD
+      metricE
+      metricF
+      metricG
+      metricH
+      metricI
+      metricJ
+      metricK
+      metricL
+      metricM
+      metricN
+      metricO
+      metricP
+    }
+  }
+`
---
+++ front-end/src/graphql/types.ts
@@ -0,0 +1,25 @@
+export type SongsDataResponse = Song[];
+
+export type Song = {
+    song: String
+    artist: String
+    songReleaseDate: String
+    playCount: String
+    metricA: String
+    metricB: String
+    metricC: String
+    metricCi: String
+    metricD: String
+    metricE: String
+    metricF: String
+    metricG: String
+    metricH: String
+    metricI: String
+    metricJ: String
+    metricK: String
+    metricL: String
+    metricM: String
+    metricN: String
+    metricO: String
+    metricP: String
+};
